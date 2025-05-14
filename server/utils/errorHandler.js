0985b55519c2d0582db4aefc93a4467c167c0952
m-afnan2018 <m.afnan2018@gmail.com>
2025-05-14 12:01:59 +0530

    Final Updated

--- server/utils/errorHandler.js
+++ server/utils/errorHandler.js
@@ -5,7 +5,6 @@ const error = (message, code)=>{
 }
 
 const failed = (response, error)=>{
-    console.log(error)
     response.status(error.code ? error.code : 500).json({
         success: false,
         message: error.message,
